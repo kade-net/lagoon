a4b06cb038916ebb4595585f02dc064fb23f31f1
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-05 11:09:54 +0300

    feat: Initing error handler

---
+++ apps/reader/romanTest.ts
@@ -0,0 +1,26 @@
+import "dotenv/config"
+import _ from "lodash"
+const { isUndefined } = _
+import { LevelDB } from "./db"
+import { ReadProcessor } from "./workers/grpc-worker/read.processor"
+import { Worker } from "./workers/grpc-worker/worker"
+
+
+try {
+    const db = await LevelDB.init()
+    const worker = new Worker(
+        process.env.INDEXER_API_KEY!,
+        db,
+        new ReadProcessor()
+    )
+    const starting = process.env.STARTING_VERSION!
+    const parsed = starting ? parseInt(starting) : undefined
+    if (Number.isNaN(parsed) || isUndefined(parsed)) {
+        throw new Error("Invalid starting version")
+    }
+    console.log("Starting worker with version", parsed)
+    await worker.run(BigInt(parsed))
+}
+catch (e) {
+    console.log("Something went wrong while processing data:", e)
+}
