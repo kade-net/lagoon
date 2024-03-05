174f910e63a72973254c549363de9c17d4a704ba
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-05 18:16:33 +0300

    retrying code on error

---
+++ apps/reader/workers/error-worker/helpers.ts
@@ -0,0 +1,63 @@
+import { ProcessorPlugin } from "../replicate-worker/helpers";
+import { ProcessMonitor } from "../replicate-worker/monitor";
+import { AccountCreatePlugin, AccountFollowPlugin, AccountUnFollowPlugin, DelegateCreatePlugin, DelegateRemovePlugin, ProfileUpdatePlugin } from "../replicate-worker/plugins/accounts";
+import { CommentCreateEventPlugin, CommentRemoveEventPlugin, PublicationCreateEventPlugin, PublicationRemoveEventPlugin, QuoteCreateEventPlugin, QuoteRemoveEventPlugin, ReactionCreateEventPlugin, ReactionRemoveEventPlugin, RepostCreateEventPlugin } from "../replicate-worker/plugins/publications";
+import { RegisterUsernamePlugin } from "../replicate-worker/plugins/usernames";
+
+export function getPlugin(type: string): ProcessorPlugin | undefined {
+    if (type === `RegisterUsernameEvent`) {
+        return new RegisterUsernamePlugin();
+    } else if (type === 'AccountCreateEvent') {
+        return new AccountCreatePlugin();
+    } else if (type === 'DelegateCreateEvent') {
+        return new DelegateCreatePlugin();
+    } else if (type === 'DelegateRemoveEvent') {
+        return new DelegateRemovePlugin();
+    } else if (type === 'AccountFollowEvent') {
+        return new AccountFollowPlugin();
+    } else if (type === 'AccountUnFollowEvent') {
+        return new AccountUnFollowPlugin();
+    } else if (type == 'ProfileUpdateEvent') {
+        return new ProfileUpdatePlugin();
+    } else if (type === 'PublicationCreate') {
+        return new PublicationCreateEventPlugin();
+    } else if (type === 'PublicationRemove') {
+        return new PublicationRemoveEventPlugin();
+    } else if (type === 'CommentCreateEvent') {
+        return new CommentCreateEventPlugin();
+    } else if (type === 'CommentRemoveEvent') {
+        return new CommentRemoveEventPlugin();
+    } else if (type === 'RepostCreateEvent') {
+        return new RepostCreateEventPlugin();
+    } else if (type === 'RepostRemoveEvent') {
+        return new RepostCreateEventPlugin();
+    } else if (type === 'QuoteCreateEvent') {
+        return new QuoteCreateEventPlugin();
+    } else if (type === 'QuoteRemoveEvent') {
+        return new QuoteRemoveEventPlugin();
+    } else if (type === 'ReactionCreateEvent') {
+        return new ReactionCreateEventPlugin();
+    } else if (type == 'ReactionRemoveEvent') {
+        return new ReactionRemoveEventPlugin();
+    }
+}
+
+export async function retry(event: string | undefined, monitor: ProcessMonitor, sequenceNumber: string) {
+    if (event) {
+        try {
+            // Parse event to get event type
+            const data = JSON.parse(event);
+            const type = data['type']
+            const eventdata = JSON.parse(data['event']);
+            let plugin = getPlugin(type);
+            
+            if (plugin) {
+                await plugin.process(eventdata, monitor, sequenceNumber);
+                monitor.setSuccess(sequenceNumber);
+            }
+        } catch(err) {
+            console.log("Error while retrying: ", err);
+            monitor.setFailed(sequenceNumber, `Error while retrying ${err}`);
+        }
+    }
+}
--- apps/reader/workers/error-worker/main.ts
+++ apps/reader/workers/error-worker/main.ts
@@ -1,10 +1,9 @@
 import { LevelDB } from "../../db";
 import { LamaReader } from "../../db/lama";
-import dataProcessor from "../replicate-worker";
 import { sleep } from "../replicate-worker/helpers";
 import { ProcessMonitor } from "../replicate-worker/monitor";
-import { RegisterUsernamePlugin } from "../replicate-worker/plugins/usernames";
 import { PostgresErrors, parsePostgresErrorType } from "./classify_error";
+import { retry } from "./helpers";
 
 const levelDB = await LevelDB.init();
 const monitor = await ProcessMonitor.init();
@@ -19,33 +18,32 @@ reader.on('data', async (data) => {
     if (event.includes("{")) {
         const data = JSON.parse(event);
         console.log(data);
-        const postgresErrorType = parsePostgresErrorType(data)
-
-
-        if(postgresErrorType === PostgresErrors.ConnectionError) {
-            // retry
-        } else if ([PostgresErrors.InsufficientResources, PostgresErrors.ProgramLimitExceeded, PostgresErrors.InvalidCursorTransactionState, PostgresErrors.OtherError].includes(postgresErrorType)) {
-            // retry after a pause
-        } else if ([PostgresErrors.ForeignKeyViolation, PostgresErrors.IntegrityViolation].includes(postgresErrorType)) {
-            // retry after a pause
-        } else if ([PostgresErrors.DataExceptionError, PostgresErrors.NotNullViolation].includes(postgresErrorType)) {
-            // somehow get data again?
-        } else if (postgresErrorType == PostgresErrors.UniqueViolation) {
-            // ignore
-        }
-
-        // // Some how get the key I need
-        // const key = data['key'];
-
-        // // Get event using the key
-        // const eventData = await levelDB.get(key);
-
-        // // And maybe call processor again after some time
-        // await sleep(180_000);
+        const postgresErrorType = parsePostgresErrorType(data['value']);
         
-        // if (eventData['type'] === "RegisterUsernameEvent") {
-        //     let plugin = new RegisterUsernamePlugin()
-        //     plugin.process(eventData, monitor, levelDB.getSequenceNumber());
-        // }
+        // Some how get the key I need
+        const key = data['key'];
+
+        // Get event using the key
+        const eventData = await levelDB.get(key);
+
+        if(eventData) {
+            if (postgresErrorType === PostgresErrors.ConnectionError) {
+                // retry
+                await sleep(60_000);
+                await retry(eventData, monitor, key);
+            } else if ([PostgresErrors.InsufficientResources, PostgresErrors.ProgramLimitExceeded, PostgresErrors.InvalidCursorTransactionState, PostgresErrors.OtherError].includes(postgresErrorType)) {
+                // retry after a pause
+                await sleep(180_000);
+                await retry(eventData, monitor, key);
+            } else if ([PostgresErrors.ForeignKeyViolation, PostgresErrors.IntegrityViolation].includes(postgresErrorType)) {
+                // retry after a pause
+                await sleep(180_000);
+                await retry(eventData, monitor, key);
+            } else if ([PostgresErrors.DataExceptionError, PostgresErrors.NotNullViolation].includes(postgresErrorType)) {
+                // somehow get data again?
+            } else if (postgresErrorType == PostgresErrors.UniqueViolation) {
+                // ignore
+            }
+        }
     }
 });
