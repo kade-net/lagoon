8a59f811abbaf0d401fd4e0361ea2964079ed99c
this-isdon <donald.onyino@globallinkplus.com>
2024-03-05 08:41:02 +0300

    chore: added logging for last read

--- apps/reader/workers/replicate-worker/main.ts
+++ apps/reader/workers/replicate-worker/main.ts
@@ -6,6 +6,7 @@ try {
     const last_read = await monitor.last_read.get("last_read")
     const _last_read = last_read ? parseInt(last_read) : 0
     const value = Number.isNaN(_last_read) ? 0 : _last_read
+    console.log("Last read::", value)
     await dataProcessor.process(value)
     // TODO: add a prune worker to remove old data
 }
