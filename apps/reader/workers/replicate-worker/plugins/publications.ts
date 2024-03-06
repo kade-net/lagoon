c1abb293acdf17ffebbb7f958a5d924c0f82b869
this-isdon <donald.onyino@globallinkplus.com>
2024-03-06 12:03:44 +0300

    chore: added content check for reposts

--- apps/reader/workers/replicate-worker/plugins/publications.ts
+++ apps/reader/workers/replicate-worker/plugins/publications.ts
@@ -20,7 +20,7 @@ export class PublicationCreateEventPlugin extends ProcessorPlugin {
 
             try {
                 await oracle.insert(publication).values({
-                    content: JSON.parse(data.payload),
+                    content: data.type == 4 ? undefined : JSON.parse(data.payload),
                     creator_id: data.user_kid,
                     id: data.kid,
                     timestamp: data.timestamp,
