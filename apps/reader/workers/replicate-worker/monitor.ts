2752d341c6803625e1c2d34139cecb1cbcf2cdb0
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-14 22:39:06 +0300

    feat: Defining monitor methods that use posthog

--- apps/reader/workers/replicate-worker/monitor.ts
+++ apps/reader/workers/replicate-worker/monitor.ts
@@ -1,4 +1,6 @@
+import { capture_event } from "posthog";
 import { Lama } from "../../db/lama";
+import { PostHogAppID, PosthogEvents } from "../../posthog/events";
 
 
 
@@ -29,7 +31,15 @@ export class ProcessMonitor {
         return this.failed.put(sequence_number, payload)
     }
 
+    setPosthogFailed(sequence_number: string, payload: Record<string, any>) {
+        capture_event(PostHogAppID, PosthogEvents.FAILED, payload);
+    }
+
     setSuccess(sequence_number: string) {
         return this.success.put(sequence_number, "success")
+    }
+
+    setPosthogSuccess(sequence_number: string, payload: Record<string, any>) {
+        capture_event(PostHogAppID, PosthogEvents.SUCCESS, payload);
     }
 }
