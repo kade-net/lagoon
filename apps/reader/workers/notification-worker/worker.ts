3c9cabd59326d7dc39f55db4dd10fcfaca9b4929
Roman Njoroge <roman.njoroge@njuguna.com>
2024-03-18 11:14:28 +0300

    feat: Defined worker that will ingest events from store

---
+++ apps/reader/workers/notification-worker/worker.ts
@@ -0,0 +1,78 @@
+import lmdb from "node-lmdb";
+import {ProcessorPlugin, sleep} from "../replicate-worker/helpers";
+import {isNull} from "lodash";
+import {capture_event} from "posthog";
+import {PostHogAppID, PosthogEvents} from "../../posthog/events";
+import {ProcessMonitor} from "../replicate-worker/monitor";
+
+export class NotificationProcessor {
+  // Store environment
+  private env: lmdb.Env;
+  private dbi: lmdb.Dbi;
+  private registeredPlugins: ProcessorPlugin[] = [];
+  private monitor: ProcessMonitor
+  
+  constructor(env: lmdb.Env, dbi: lmdb.Dbi, monitor: ProcessMonitor) {
+    this.env = env;
+    this.dbi = dbi;
+    this.monitor = monitor
+  }
+
+  addRegisterPlugin(plugin: ProcessorPlugin) {
+    this.registeredPlugins.push(plugin);
+  }
+
+
+  async process(_last_read?: number) {
+
+    let last_read = "000000000";
+
+    // Processing the last read key
+    if (_last_read) {
+      const s = `000000000${_last_read}`;
+      last_read = s.substring(s.length - 9);
+    }
+
+    // Creating cursor to read store with
+    const txn = this.env.beginTxn({
+      readOnly: true
+    })
+    const cursor = new lmdb.Cursor(txn, this.dbi)
+
+    const atRange = last_read === "000000000" ? {} : cursor.goToRange(last_read);
+
+    // Seeing if there is anything
+    if (!atRange) {
+      cursor.close();
+      txn.commit();
+      await sleep(60_000);
+      await this.process(parseInt(last_read));
+      return;
+    }
+
+    let key, value: Buffer | null;
+    while((key = cursor.goToNext()) != null) {
+      // Gets data at key
+      value = cursor.getCurrentBinary();
+      
+      if(value && !isNull(value)) {
+        const data = JSON.parse(value.toString());
+        const event_type = data.type;
+        const event_data = JSON.parse(data.event);
+        const signature = data.signature;
+
+        const chosenPlugin = this.registeredPlugins.find(p => p.name === event_type)
+
+        if (chosenPlugin) {
+          try {
+            await chosenPlugin.process(event_data, this.monitor, key, signature);
+          } catch(e) {
+            capture_event(PostHogAppID, PosthogEvents.FAILED, {error: e});
+          }
+      }
+  
+    }
+    
+  }
+  }
+}
