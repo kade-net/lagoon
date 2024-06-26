292f8a587610620b84b96998600311ec3a1c21b4
Roman Njoroge <roman.njoroge@njuguna.com>
2024-06-26 08:29:34 +0300

    feat: New migrations

---
+++ apps/oracle/db/migrations/0004_wealthy_squadron_sinister.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "follow" DROP CONSTRAINT "follow_follower_id_account_id_fk";
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "follow" ADD CONSTRAINT "follow_follower_id_account_id_fk" FOREIGN KEY ("follower_id") REFERENCES "account"("id") ON DELETE no action ON UPDATE cascade;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
